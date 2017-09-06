491c973f0a131448e0774ad672faf5045173b3fe
Enrique Duran <IngQuikemon@users.noreply.github.com>
2017-09-05 20:03:49 -0500

    feat: Implementing routing to app
    Implemented the foundation of the routing as well as the main distribiuton of the main navigation parts of the app.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { Grid, Row, Col, PageHeader, ListGroup, ListGroupItem, Badge, Nav, NavItem, Panel } from 'react-bootstrap';
-import PostList from './PostList'
+import {Route, Link} from 'react-router-dom';
+import PostList from './PostList';
 
 class App extends Component {
   state = {
@@ -14,14 +15,21 @@ class App extends Component {
             <Col md={10}>
               <PageHeader> Readable <small>a Udacity project</small></PageHeader>
               <div>
-                <PostList posts={this.state.posts}/>
+                <Route exact path="/" render{() =>(
+                    <PostList posts={this.state.posts}/>
+                  )} />
+                <Route path="/:category" component={PostList} />
+                <Route path="/Post/:id" render{() =>(
+                    <PostView />
+                )} />
               </div>
             </Col>
             <Col md={2}>
               <div className="menu_holder">
                 <h3>Categories:</h3>
                 <Nav bsStyle="pills" stacked >
-                  <NavItem eventKey={1} onSelect={() => {this.active = true}} active>All</NavItem>
+                  //<Link to={`/roster/${p.number}`}>{p.name}</Link>
+                  <NavItem eventKey={1} onSelect={() => {this.active = true}} active="true">All</NavItem>
                   <NavItem eventKey={2} onSelect={() => {this.active = true}}>Category 1</NavItem>
                   <NavItem eventKey={3} onSelect={() => {this.active = true}}>Category 2</NavItem>
                   <NavItem eventKey={4} onSelect={() => {this.active = true}}>Category 3</NavItem>
